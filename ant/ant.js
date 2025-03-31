5f8322361a549a393401576fefb34bde2ab00d55
2ec_End <leealex.2006@mail.ru>
2025-03-31 13:23:53 +0700

    added graph rendering

--- ant/ant.js
+++ ant/ant.js
@@ -1,13 +1,33 @@
+var points = [];
+
 var canvas = document.getElementsByTagName('canvas')[0];
 canvas.height = 480;
 canvas.width  = 640;
 
-var contex = example.getContext('2d');
-canvas.addEventListener('click', function(e){
+var context = example.getContext('2d');
+canvas.addEventListener('click', function(e)
+{
+    let currX = e.x - 8, currY = e.y - 8;
+
     // Отрисовка точки в месте клика 
     // Сдвиг на 8 нужен для того, центр точки был ровно на курсоре
-    contex.beginPath();
-    contex.arc(e.x - 8, e.y - 8, 5, 0, 2 * Math.PI, true);
-    contex.fill();
-    contex.closePath();
+    context.fillStyle = "#5a5a5a";
+    context.beginPath();
+    context.arc(currX, currY, 5, 0, 2 * Math.PI, true);
+    context.fill();
+    context.closePath();
+
+    // Пушим точку в массив
+    points.push([currX, currY]);
+
+    let index = 0;
+    context.strokeStyle = "rgba(120, 120, 120, 0.1)";
+    while (index < points.length - 1)
+    {
+        context.moveTo(currX, currY);
+        context.lineTo(points[index][0], points[index][1]);
+        context.stroke();
+
+        index++;
+    }
 });
