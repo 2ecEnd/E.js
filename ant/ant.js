ba91c2cea142c4366338ea07f3267f5945e50d27
2ec_End <leealex.2006@mail.ru>
2025-03-31 14:35:13 +0700

    creatind adj matrix for alg

--- ant/ant.js
+++ ant/ant.js
@@ -1,3 +1,4 @@
+//Работа с холстом
 var points = [];
 
 var canvas = document.getElementsByTagName('canvas')[0];
@@ -7,10 +8,10 @@ canvas.width  = 640;
 var context = example.getContext('2d');
 canvas.addEventListener('click', function(e)
 {
+    // Сдвиг на 8 нужен для того, центр точки был ровно на курсоре
     let currX = e.x - 8, currY = e.y - 8;
 
     // Отрисовка точки в месте клика 
-    // Сдвиг на 8 нужен для того, центр точки был ровно на курсоре
     context.fillStyle = "#5a5a5a";
     context.beginPath();
     context.arc(currX, currY, 5, 0, 2 * Math.PI, true);
@@ -29,5 +30,29 @@ canvas.addEventListener('click', function(e)
         context.stroke();
 
         index++;
+    }
+});
+
+
+// Активация алгоритма
+var button = document.getElementById('start');
+button.addEventListener('click', function(e)
+{
+    // Составление матрицы смежности
+    adj = [];
+    for(let i in points)
+    {
+        let row = []
+        for(let j in points)
+            row.push(((points[i][0] - points[j][0]) ** 2 + (points[i][1] - points[j][1]) ** 2) ** (0.5))
+        adj.push(row);
+    }
+
+    for(let i in points)
+    {
+        let row = '';
+        for(let j in points)
+            row += ' ' + adj[i][j].toFixed(3);
+        console.log(row);
     }
 });
