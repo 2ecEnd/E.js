7f9c978a20f20cabfb1f83538d13be2f63676c60
Several_SIZE <kner647@gmail.com>
2025-04-26 02:03:11 +0700

    Улучшил интерфейс и добавил комментарии

--- aStar/maze.js
+++ aStar/maze.js
@@ -1,52 +1,59 @@
-
+class mazeCell{ // Так как мой лабиринт генерируется с шагом 2, мне нужна не только клетка в которую я попадаю, но и клетка перед ней - path cell
+    constructor(targetCell, pathCell){
+        this.targetCell = targetCell;
+        this.pathCell = pathCell;
+    }
+}
 
 function generateMaze(){
     toggleAllCellsIntoWalls(); // Преобразование все клетки в стены
     let randomCell = chooseRandomCell();
-    let startCell = [randomCell, randomCell]; // 
-    let reachable = [startCell]; // Очередь доступных клеток, состоит
+    let startCell = new mazeCell(randomCell, randomCell);
+    let reachable = [startCell]; // Очередь доступных клеток
 
     while(reachable.length > 0){
         
-        let currentCellIndex = Math.floor(Math.random() * reachable.length);
-        let currentCellNumber = reachable[currentCellIndex][0];
+        let currentCellIndex = Math.floor(Math.random() * reachable.length); // получаю случайную клетки
+        let currentCellNumber = reachable[currentCellIndex].targetCell; // это именно та клетка которую выбирает алгоритм
 
-        if (graphNodes[currentCellNumber].isWall == false){
+        if (graphNodes[currentCellNumber].isWall == false){ // проверяем не стена ли она
             reachable.splice(currentCellIndex, 1);
             continue;
         }
-        let pathCell = reachable[currentCellIndex][1];
+        let pathCell = reachable[currentCellIndex].pathCell; // это клетка перед нужной, её тоже нужно превратить в свободную
         reachable.splice(currentCellIndex, 1);
 
 
-        graphNodes[currentCellNumber].isWall = false;
-        const cell = document.getElementById(currentCellNumber);
-        cell.classList.remove('wall');
-        cell.classList.add('free');
-
-        graphNodes[pathCell].isWall = false;
-        const prevCell = document.getElementById(pathCell);
-        prevCell.classList.remove('wall');
-        prevCell.classList.add('free');
+        toggleCellIntoFree(currentCellNumber);
+        toggleCellIntoFree(pathCell);
 
 
+        // далее идут проверки на доступность соседних клеток, если они являются стенами и не выходят за границы карты, то они добвляются в очередь
         if ((currentCellNumber%n - (currentCellNumber-2)%n == 2) && (currentCellNumber-2 >= 0) && (graphNodes[currentCellNumber-2].isWall == true)){
-            const turn = [currentCellNumber-2, currentCellNumber-1];
+            const turn = new mazeCell(currentCellNumber-2, currentCellNumber-1);
             reachable.push(turn);
         }
         if (((currentCellNumber+2)%n - currentCellNumber%n == 2) && (currentCellNumber+2 < n*n) && (graphNodes[currentCellNumber+2].isWall == true)){
-            const turn = [currentCellNumber+2, currentCellNumber+1];
+            const turn = new mazeCell(currentCellNumber+2, currentCellNumber+1);
             reachable.push(turn);
         }
         if((currentCellNumber-2*n >= 0) && (graphNodes[currentCellNumber-2*n].isWall == true)){
-            const turn = [currentCellNumber-2*n, currentCellNumber-n];
+            const turn = new mazeCell(currentCellNumber-2*n, currentCellNumber-n);
             reachable.push(turn);
         }
         if((currentCellNumber+2*n < n*n) && (graphNodes[currentCellNumber+2*n].isWall == true)){
-            const turn = [currentCellNumber+2*n, currentCellNumber+n];
+            const turn = new mazeCell(currentCellNumber+2*n, currentCellNumber+n);
             reachable.push(turn);
         }
     }
+}
+
+
+function toggleCellIntoFree(CellNumber){
+    graphNodes[CellNumber].isWall = false;
+    const cell = document.getElementById(CellNumber);
+    cell.classList.remove('wall');
+    cell.classList.add('free');
 }
 
 
